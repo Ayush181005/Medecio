ca017e7eeaab73d20b793c60aa2ccefa33991f4d
Ayush <ayush.singh18@outlook.com>
2023-09-24 11:04:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function doctor_signup(){
+    window.location.href='doctor_login.php'
+}
+
+
+
+var sidemenu=document.getElementById("sidemenu");
+
+        function openmenu(){
+            sidemenu.style.left="0";
+        }
+        function closemenu(){
+            sidemenu.style.left="-200px";
+        }
