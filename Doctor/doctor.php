96302da37c9770e9c5e2c8ed4891c5a88eea42f5
DeepMathukiya <dsmathukiya2004@gmail.com>
2023-09-30 11:45:03 +0530

    .

--- Doctor/doctor.php
+++ Doctor/doctor.php
@@ -117,7 +117,7 @@ cursor: pointer;
                 </tr>
                  -->
             <?php
-            include '../connection.php';
+            include '../connection.php'; 
             if(isset($_POST['submit'])){
               ?>
 
